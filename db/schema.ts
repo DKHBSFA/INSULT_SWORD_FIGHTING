e3a22251e0eb3aad9d60147109aace26ddc90f42
EfM8caQE9F1v4pFt5x7WrjDI1N2kFqYdydVj <116493713+EfM8caQE9F1v4pFt5x7WrjDI1N2kFqYdydVj@users.noreply.github.com>
2026-05-01 18:02:49 +0200

    chore: add Drizzle ORM + drizzle-kit config

---
+++ db/schema.ts
@@ -0,0 +1,9 @@
+import { sqliteTable, text, integer, index, uniqueIndex } from 'drizzle-orm/sqlite-core';
+import { sql } from 'drizzle-orm';
+
+void sqliteTable;
+void text;
+void integer;
+void index;
+void uniqueIndex;
+void sql;
